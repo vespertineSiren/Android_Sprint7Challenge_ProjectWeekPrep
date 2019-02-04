3e1e0bf886f8f85fac070eb720328a395b6ae074
Patrick Martin <patrickmartin88@gmail.com>
2019-02-04 12:52:07 -0800

    Complete details layout. Will turn imagebuttons in layout into a custom view.

---
+++ app/src/main/java/com/fatehistory/patrickjmartin/fatehistory/HistoricalFiguresDetails.java
@@ -0,0 +1,13 @@
+package com.fatehistory.patrickjmartin.fatehistory;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HistoricalFiguresDetails extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_historical_figures_details);
+    }
+}
