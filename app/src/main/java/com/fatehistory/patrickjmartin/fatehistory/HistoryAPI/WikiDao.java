a7fe0e55a40ce39de00159f4181dae164504e94c
Patrick Martin <patrickmartin88@gmail.com>
2019-02-05 16:00:33 -0800

    Finalized how images will be pulled. Implementation of favorites fragment.

---
+++ app/src/main/java/com/fatehistory/patrickjmartin/fatehistory/HistoryAPI/WikiDao.java
@@ -0,0 +1,28 @@
+package com.fatehistory.patrickjmartin.fatehistory.HistoryAPI;
+
+public class WikiDao {
+
+    private static String BASE_FATE_BIO_URL =
+            "https://fategrandorder.fandom.com/api.php?action=query&prop=revisions&rvprop=content&format=json&titles=";
+
+    private static final String FATE_BIO_URL = BASE_FATE_BIO_URL + "%d";
+
+    private static String BASE_FATE_IMAGE_URL =
+            "https://fategrandorder.fandom.com/api.php?action=query&format=json&list=allimages&aisort=name&aifrom=";
+
+    private static final String FATE_IMAGES_URL = BASE_FATE_IMAGE_URL + "%d";
+
+    //Might not need this since BASE_BIOGRAPHY_URL provides tags
+    private static String BASE_INFOBOX_URL =
+            "https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro&explaintext&redirects=1&titles=";
+
+    private static final String HISTORICAL_INFBOX_URL = BASE_INFOBOX_URL + "%d";
+
+    private static String BASE_IMAGE_URL =
+            "https://en.wikipedia.org/w/api.php?action=query&prop=pageimages&format=json&piprop=original&titles=";
+
+    private static final String HISTORICAL_IMAGE_URL = BASE_IMAGE_URL + "%d";
+
+
+
+}
