58318ca703ee0c496bf721cf39c4168dcee54b1a
Patrick Martin <patrickmartin88@gmail.com>
2019-02-06 19:16:38 -0800

    Finalized fragments that will be used in viewpager.

---
+++ app/src/main/java/com/fatehistory/patrickjmartin/fatehistory/PastViewedHistoricalFigureRecyclerViewAdapter.java
@@ -0,0 +1,72 @@
+package com.fatehistory.patrickjmartin.fatehistory;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.fatehistory.patrickjmartin.fatehistory.HistoryAPI.HistoricalFigure;
+import com.fatehistory.patrickjmartin.fatehistory.PastViewedFragment.OnListFragmentInteractionListener;
+
+
+import java.util.List;
+
+
+public class PastViewedHistoricalFigureRecyclerViewAdapter extends RecyclerView.Adapter<PastViewedHistoricalFigureRecyclerViewAdapter.ViewHolder> {
+
+    private final List<HistoricalFigure> mValues;
+    private final OnListFragmentInteractionListener mListener;
+
+    public PastViewedHistoricalFigureRecyclerViewAdapter(List<HistoricalFigure> items, OnListFragmentInteractionListener listener) {
+        mValues = items;
+        mListener = listener;
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.fragment_pastviewed, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(final ViewHolder holder, int position) {
+        holder.mItem = mValues.get(position);
+
+        holder.mView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (null != mListener) {
+                    // Notify the active callbacks interface (the activity, if the
+                    // fragment is attached to one) that an item has been selected.
+
+                }
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return mValues.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        public final View mView;
+        public final TextView mIdView;
+        public final TextView mContentView;
+        public HistoricalFigure mItem;
+
+        public ViewHolder(View view) {
+            super(view);
+            mView = view;
+            mIdView = (TextView) view.findViewById(R.id.item_number);
+            mContentView = (TextView) view.findViewById(R.id.content);
+        }
+
+        @Override
+        public String toString() {
+            return super.toString() + " '" + mContentView.getText() + "'";
+        }
+    }
+}
