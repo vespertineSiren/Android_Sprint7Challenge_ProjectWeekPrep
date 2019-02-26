86859ad812ffbf91cbd8756f323d588fa87ff276
Patrick Martin <patrickmartin88@gmail.com>
2019-02-26 11:30:25 -0800

    Implement HistoricalFigureDatabase using Room.

--- app/src/main/java/com/fatehistory/patrickjmartin/fatehistory/HistoryAPI/HistoricalFigureDatabase.java
+++ app/src/main/java/com/fatehistory/patrickjmartin/fatehistory/HistoryAPI/HistoricalFigureDatabase.java
@@ -1,8 +1,33 @@
 package com.fatehistory.patrickjmartin.fatehistory.HistoryAPI;
 
 import android.arch.persistence.room.Database;
+import android.arch.persistence.room.Room;
 import android.arch.persistence.room.RoomDatabase;
+import android.content.Context;
 
 @Database(entities = {HistoricalFigure.class}, version= 1)
 public abstract class HistoricalFigureDatabase extends RoomDatabase {
+
+    public abstract HistoricalFigureDAO getHistoricalFigureDAO();
+
+    private static HistoricalFigureDatabase historicalFigureDB;
+
+    public static HistoricalFigureDatabase getInstance(Context context) {
+        if(historicalFigureDB == null) {
+            historicalFigureDB = buildDatabaseInstance(context);
+        }
+
+        return historicalFigureDB;
+    }
+
+    private static HistoricalFigureDatabase buildDatabaseInstance(Context context) {
+        return Room.databaseBuilder(context,
+                HistoricalFigureDatabase.class,
+                "historicalfiguredb.db").build();
+    }
+
+    public void cleanUp(){
+        historicalFigureDB = null;
+    }
+
 }
